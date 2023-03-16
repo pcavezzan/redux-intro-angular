4ae974390025356725f26ddb763e8fa59ad5d5ad
Patrice CAVEZZAN <pcavezzan@gmail.com>
2023-03-16 01:00:24 +0100

    Step 4 - Custom store.
    On mets en avant l'analogie avec l'utilisation d'un état et comment on interragit sur cette état.

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -2,26 +2,42 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {map, Observable, Subject} from "rxjs";
 
+// AKA: mélange d'un store et side effects
 @Injectable({providedIn: 'root'})
 export class MessageService {
   private messageApiUrl = '/api/messages';
 
+  // AKA: state
   private messagesSubject: Subject<Message[]> = new Subject<Message[]>();
+
+  // AKA: selectors
   messages$: Observable<Message[]> = this.messagesSubject.asObservable();
   messagesCount$: Observable<number> = this.messagesSubject.pipe(map((messages) => messages.length));
 
   constructor(private httpClient: HttpClient) {
   }
 
+  // AKA: actions
   findAll(): void {
-    this.httpClient.get<Message[]>(this.messageApiUrl).subscribe(
-      messages => this.messagesSubject.next(messages)
-    );
+    // AKA: side effects
+    this.httpClient.get<Message[]>(this.messageApiUrl).subscribe(messages => {
+      // AKA: actions
+      this.onReceiveMessagesSuccess(messages)
+    });
   }
 
+  // AKA: actions
   create(message: Message): void {
-    this.httpClient.post<Message>(this.messageApiUrl, message).subscribe((message) => {
+    // AKA: side effects
+    this.httpClient.post<Message>(this.messageApiUrl, message).subscribe((_) => {
+      // AKA: actions
       this.findAll();
     });
+  }
+
+  // AKA: actions
+  private onReceiveMessagesSuccess(messages: Message[]) {
+    // AKA: setState <=> reducer
+    this.messagesSubject.next(messages);
   }
 }
