9fc5928105264831d8b056426b893e8c1ee812f9
Patrice CAVEZZAN <pcavezzan@gmail.com>
2023-03-16 20:29:44 +0100

    Step 2 - Illustration du problème à partir d'un vrai server.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,9 @@ import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {MessagesComponent} from './messages/messages.component';
 import {HttpClientModule} from "@angular/common/http";
-import {HttpClientInMemoryWebApiModule} from "angular-in-memory-web-api";
-import {InMemoryMessageDataServerService} from "./in-memory-message-data-server.service";
 import {ReactiveFormsModule} from "@angular/forms";
-import { MessageCreateComponent } from './message-create/message-create.component';
-import { MessageFormComponent } from './message-form/message-form.component';
+import {MessageCreateComponent} from './message-create/message-create.component';
+import {MessageFormComponent} from './message-form/message-form.component';
 
 @NgModule({
   declarations: [
@@ -23,10 +21,6 @@ import { MessageFormComponent } from './message-form/message-form.component';
     BrowserModule,
     ReactiveFormsModule,
     HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryMessageDataServerService,
-      {dataEncapsulation: false}
-    )
   ],
   providers: [],
   bootstrap: [AppComponent]
