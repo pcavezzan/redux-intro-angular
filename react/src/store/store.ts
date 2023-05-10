0d5ecad11606960c1e2fbe18d3e2c605200aea78
Patrice CAVEZZAN <pcavezzan@gmail.com>
2023-05-10 20:15:12 +0200

    Mise en en place de redux-observable et suppression redux-saga.

--- react/src/store/store.ts
+++ react/src/store/store.ts
@@ -1,18 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import createSagaMiddleware from 'redux-saga';
 import { rootReducer } from './reducer';
-import rootSaga from './saga';
-
-// create the saga middleware
-const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
-  reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(sagaMiddleware)
+  reducer: rootReducer
 });
-// then run the saga
-sagaMiddleware.run(rootSaga);
-
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
