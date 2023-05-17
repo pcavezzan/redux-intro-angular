3289c483ace8db484c01f8fa78a9209fcb3d57eb
Patrice CAVEZZAN <pcavezzan@gmail.com>
2023-05-17 23:56:18 +0200

    Mise en place de rxjs marbles.

--- react/src/store/messages/messages.epics.test.ts
+++ react/src/store/messages/messages.epics.test.ts
@@ -1,9 +1,9 @@
-import { loadMessagesAction, loadMessagesSuccessAction } from './messages.actions';
-import * as service from './messages.epics.adapter';
 import { Subject } from 'rxjs';
 import { StateObservable } from 'redux-observable';
 import { RootState } from '../store';
 import { messagesEpics } from './messages.epics';
+import { createMessageAction, loadMessagesAction, loadMessagesSuccessAction } from './messages.actions';
+import * as service from './messages.epics.adapter';
 import { cold, hot } from 'jest-marbles';
 
 describe('messagesEpics', () => {
@@ -12,12 +12,24 @@ describe('messagesEpics', () => {
   const state$ = new StateObservable(new Subject<RootState>(), initialState);
 
   test('should dispatch load messages success action on load messages action', () => {
-    const messages = [{id: 1, content: 'content'}];
-    jest.spyOn(service, 'findAllMessages$').mockReturnValue(cold('-a', {a: messages}));
-    const actions$ = hot('-a', loadMessagesAction());
+      const messages = [{id: 1, content: 'content'}];
+      jest.spyOn(service, 'findAllMessages$').mockReturnValue(cold('-a', {a: messages}));
+      const actions$ = hot('-a', {a: loadMessagesAction()});
 
-    const expectedActions$ = messagesEpics(actions$, state$, null);
+      const expectedActions$ = messagesEpics(actions$, state$, null);
 
-    expect(expectedActions$).toBeObservable(cold('--r', {r: loadMessagesSuccessAction(messages)}));
-  });
+      expect(expectedActions$).toBeObservable(cold('--r', {r: loadMessagesSuccessAction(messages)}));
+    }
+  );
+
+  test('should dispatch load messages action on create message action', () => {
+      const message = {id: 1, content: 'content'};
+      jest.spyOn(service, 'createMessage$').mockReturnValue(cold('-a', {a: {}}));
+      const actions$ = hot('-a', {a: createMessageAction(message)});
+
+      const expectedActions$ = messagesEpics(actions$, state$, null);
+
+      expect(expectedActions$).toBeObservable(cold('--r', {r: loadMessagesAction()}));
+    }
+  );
 });
--- react/src/store/messages/messages.epics.ts
+++ react/src/store/messages/messages.epics.ts
@@ -1,13 +1,12 @@
 import { combineEpics, ofType, StateObservable } from 'redux-observable';
 import { RootState } from '../store';
 import { createMessageAction, loadMessagesAction, loadMessagesSuccessAction, MessageActions } from './messages.actions';
-import { map, Observable, switchMap, tap } from 'rxjs';
+import { map, Observable, switchMap } from 'rxjs';
 import { ActionCreatorWithPayload } from '@reduxjs/toolkit';
 import { createMessage$, findAllMessages$ } from './messages.epics.adapter';
 
 const loadMessagesEpic = (actions$: Observable<MessageActions>, _: StateObservable<RootState>): Observable<MessageActions> => actions$.pipe(
   ofType(loadMessagesAction.type),
-  tap(() => console.log('test')),
   switchMap(() => findAllMessages$()),
   map((messages) => loadMessagesSuccessAction(messages))
 );
